7cca1ddfc60a2002c053f3e73df1873ed246cabb
VictorRodrigues16 <victorr.29@hotmail.com>
2024-09-06 09:39:06 -0300

    feat: .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,24 +1,27 @@
-import "reflect-metadata"
+import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Users } from "./entities/Users";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres", 
-  password: "123",
-  database: "cadastro_user",
-  logging: true,
+  type: process.env.TYPE_DATABASE as any, 
+  host: process.env.HOST_DATABASE, 
+  port: Number(process.env.PORT_DATABASE), 
+  username: process.env.USERNAME_DATABASE, 
+  password: process.env.PASSWORD_DATABASE, 
+  database: process.env.DATABASE, 
+  logging: process.env.NODE_ENV === 'development', 
   entities: [Users],
   migrations: [
       __dirname + '/migrations/*.ts'
   ],
   subscribers: [],
+});
 
-})
 AppDataSource.initialize()
-              .then(() => console.log('connected success'))
-              .catch((error) => console.log(error))
-              
-export default AppDataSource
+  .then(() => console.log('Conexão bem-sucedida'))
+  .catch((error) => console.log(error));
+
+export default AppDataSource;
